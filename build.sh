214c56996b7a2a73c16ff6dd3ef5a3bf44c9ccfd
Royweru <royezi17@gmail.com>
2025-04-04 09:04:05 +0300

    Made some few changes on my build file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-set -o errexit #exit on error
+set -o errexit  #Exit on error
 
 pip install -r requirements.txt
-pip manage.py collectstatic --noinput
-pip manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
